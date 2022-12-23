662d4661ab5462fa64b19b28284daf1fd8e3a051
Emerson Garrido <emersongarrido@hotmail.com.br>
2022-12-22 20:37:08 -0400

    :rocket: add icons

--- out/extension.js
+++ out/extension.js
@@ -1,2 +1,63 @@
-var v=Object.create;var c=Object.defineProperty;var w=Object.getOwnPropertyDescriptor;var u=Object.getOwnPropertyNames;var b=Object.getPrototypeOf,p=Object.prototype.hasOwnProperty;var r=e=>c(e,"__esModule",{value:!0});var x=(e,t)=>{r(e);for(var n in t)c(e,n,{get:t[n],enumerable:!0})},g=(e,t,n)=>{if(t&&typeof t=="object"||typeof t=="function")for(let o of u(t))!p.call(e,o)&&o!=="default"&&c(e,o,{get:()=>t[o],enumerable:!(n=w(t,o))||n.enumerable});return e},s=e=>g(r(c(e!=null?v(b(e)):{},"default",e&&e.__esModule&&"default"in e?{get:()=>e.default,enumerable:!0}:{value:e,enumerable:!0})),e);x(exports,{activate:()=>f,deactivate:()=>T});var i=s(require("vscode")),d=s(require("jsx-to-html")),l=s(require("@babel/core"));function f(e){console.log('Congratulations, your extension "react-email-extension" is now active!');let t=i.commands.registerCommand("extension.previewTSX",()=>{let n=i.window.createWebviewPanel("previewTSX","React Email Preview",i.ViewColumn.Two,{enableScripts:!0}),o=()=>{n.title="React Email Preview",n.webview.html=h()};i.workspace.onDidChangeTextDocument(m=>{var a;m.document===((a=i.window.activeTextEditor)==null?void 0:a.document)&&o()}),i.window.onDidChangeActiveTextEditor(()=>{o()})});e.subscriptions.push(t)}function h(){var o;let e=(o=i.window.activeTextEditor)==null?void 0:o.document.getText(),{code:t}=l.transform(e,{plugins:["jsx-to-html/babel-plugin"],blacklist:["react"]});return(0,d.render)(t)}function T(){}0&&(module.exports={activate,deactivate});
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+const vscode = __importStar(require("vscode"));
+const jsx_to_html_1 = require("jsx-to-html");
+const babel = __importStar(require("@babel/core"));
+function activate(context) {
+    console.log('Congratulations, your extension "react-email-extension" is now active!');
+    let disposable = vscode.commands.registerCommand("extension.previewTSX", () => {
+        const panel = vscode.window.createWebviewPanel("previewTSX", "React Email Preview", vscode.ViewColumn.Two, { enableScripts: true });
+        const updateWebview = () => {
+            panel.title = "React Email Preview";
+            panel.webview.html = getWebviewContent();
+        };
+        vscode.workspace.onDidChangeTextDocument((event) => {
+            var _a;
+            if (event.document === ((_a = vscode.window.activeTextEditor) === null || _a === void 0 ? void 0 : _a.document)) {
+                updateWebview();
+            }
+        });
+        vscode.window.onDidChangeActiveTextEditor(() => {
+            updateWebview();
+        });
+    });
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+function getWebviewContent() {
+    var _a;
+    const email = (_a = vscode.window.activeTextEditor) === null || _a === void 0 ? void 0 : _a.document.getText();
+    const { code } = babel.transform(email, {
+        plugins: ["jsx-to-html/babel-plugin"],
+        blacklist: ["react"],
+    });
+    const html = (0, jsx_to_html_1.render)(code);
+    return `${html}`;
+}
+function deactivate() { }
+exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,52 +1,55 @@
-import * as vscode from 'vscode'
-import { render } from 'jsx-to-html'
-import * as babel from '@babel/core'
+import * as vscode from "vscode";
+import { render } from "jsx-to-html";
+import * as babel from "@babel/core";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log(
-    'Congratulations, your extension "react-email-extension" is now active!',
-  )
+    'Congratulations, your extension "react-email-extension" is now active!'
+  );
 
-  let disposable = vscode.commands.registerCommand('extension.previewTSX', () => {
-    const panel = vscode.window.createWebviewPanel(
-      'previewTSX',
-      'React Email Preview',
-      vscode.ViewColumn.Two,
-      { enableScripts: true },
-    )
+  let disposable = vscode.commands.registerCommand(
+    "extension.previewTSX",
+    () => {
+      const panel = vscode.window.createWebviewPanel(
+        "previewTSX",
+        "React Email Preview",
+        vscode.ViewColumn.Two,
+        { enableScripts: true }
+      );
 
-    const updateWebview = () => {
-      panel.title = 'React Email Preview'
-      panel.webview.html = getWebviewContent()
+      const updateWebview = () => {
+        panel.title = "React Email Preview";
+        panel.webview.html = getWebviewContent();
+      };
+
+      // Atualiza a visualização do painel quando o código for alterado
+      vscode.workspace.onDidChangeTextDocument((event) => {
+        if (event.document === vscode.window.activeTextEditor?.document) {
+          updateWebview();
+        }
+      });
+
+      // Atualiza a visualização do painel quando o usuário mudar de aba no editor
+      vscode.window.onDidChangeActiveTextEditor(() => {
+        updateWebview();
+      });
     }
+  );
 
-    // Atualiza a visualização do painel quando o código for alterado
-    vscode.workspace.onDidChangeTextDocument((event) => {
-      if (event.document === vscode.window.activeTextEditor?.document) {
-        updateWebview()
-      }
-    })
-
-    // Atualiza a visualização do painel quando o usuário mudar de aba no editor
-    vscode.window.onDidChangeActiveTextEditor(() => {
-      updateWebview()
-    })
-  })
-
-  context.subscriptions.push(disposable)
+  context.subscriptions.push(disposable);
 }
 
 function getWebviewContent() {
   const email = vscode.window.activeTextEditor?.document.getText();
 
   const { code } = babel.transform(email, {
-    plugins: ['jsx-to-html/babel-plugin'],
-    blacklist: ['react'],
-  })
+    plugins: ["jsx-to-html/babel-plugin"],
+    blacklist: ["react"],
+  });
 
-  const html = render(code)
+  const html = render(code);
 
-  return html
+  return `${html}`;
 }
 
 export function deactivate() {}
